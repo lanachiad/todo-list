a1f46e4a5ba8b06c5942a2e4fbfd0bc67893f136
Lana Chiad <lanachiad7@gmail.com>
2018-01-01 16:58:10 -0500

    Add active state to each ToDo, triggered to true onClick

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './ToDo.css';
 
 class ToDo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+    this.addStrikeThrough = this.addStrikeThrough.bind(this);
+  }
+
+  addStrikeThrough() {
+    this.setState({ active: true });
+  }
+
   render() {
     return (
       <div className="todo-wrapper">
-        <input className="number" type="checkbox" />
+        <input className="number" type="checkbox" onClick={this.addStrikeThrough} />
         <span>
           {this.props.task}
         </span>
